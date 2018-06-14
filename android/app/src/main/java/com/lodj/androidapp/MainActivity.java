c4764155a25af29f649d483be7999d9526d91aa7
Luis Diaz <diaz.luispr@gmail.com>
2018-06-14 10:47:24 -0400

    Add Basic Android app with a React-Native View

---
+++ android/app/src/main/java/com/lodj/androidapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.lodj.androidapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToReact();
+            }
+        });
+    }
+
+    private void goToReact(){
+        Intent intent = new Intent(this, MyReactActivity.class);
+        startActivity(intent);
+    }
+}
